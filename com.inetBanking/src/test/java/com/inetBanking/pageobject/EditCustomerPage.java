c874cd1bd211eba009b5355ba94bbcc5bf91bd85
vasan <vasan@LAPTOP-BKF1645O>
2023-07-16 10:05:22 +0530

    Updated Edit Customer

--- com.inetBanking/src/test/java/com/inetBanking/pageobject/EditCustomerPage.java
+++ com.inetBanking/src/test/java/com/inetBanking/pageobject/EditCustomerPage.java
@@ -1,12 +1,23 @@
 package com.inetBanking.pageobject;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class EditCustomerPage {
 
 	WebDriver driver;
+	
+	@FindBy(name="XYZ")
+	WebElement cutomerDetails;
 
 	public EditCustomerPage(WebDriver driver) {
 		this.driver = driver;
+	}
+		
+	
+	public void editCustomerDetails()
+	{
+		
 	}
 }
